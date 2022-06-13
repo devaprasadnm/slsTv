9909a3167ce2407e7e988f473db97c59e5558026
noorhana123 <hananoorudheen721@gmail.com>
2022-06-13 12:33:00 +0530

    Images table created

--- main/models.py
+++ main/models.py
@@ -5,4 +5,5 @@ from django.db import models
 class News(models.Model):
     Title = models.CharField(max_length=100, default=None)
     Content = models.TextField()
-
+class Images(models.Model):
+    Photos = models.ImageField(upload_to ='files')
