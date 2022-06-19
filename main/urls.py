6bdefb218dec9144ef8fbe783403a5bde72182bf
Devaprasad <devaprasadmohan@gmail.com>
2022-06-19 12:19:10 +0530

    live data display using ajax implemented without image

--- main/urls.py
+++ main/urls.py
@@ -19,4 +19,5 @@ from  . import views
 
 urlpatterns = [
     path('home', views.home ,name='home'),
+    path('data', views.data ,name='data'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from .models import News,Images
+from django.http import JsonResponse
 # Create your views here.
 
 def home(request):
+    return (render(request,'main.html'))
+
+
+def data(request):
     news = News.objects.all()
     images = Images.objects.all()
-    return(render(request,'main.html',{'news':news,'images':images}))
-
+    return JsonResponse({"users": list(news.values()),"image":list(images.values())})
