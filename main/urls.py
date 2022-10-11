81dac4581bc589d8da032f3cbd324b2590ae3af3
sls <sls@sls.cusat.ac.in>
2022-10-11 18:53:06 +0530

    add server changes to main

--- main/urls.py
+++ main/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from  . import views
 
 urlpatterns = [
-    path('home', views.index ,name='index'),
+    path('', views.index ,name='index'),
     path('index_data', views.data_index ,name='data_index'),
 ]
--- slsTv/settings.py
+++ slsTv/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+#import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/tv/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
@@ -135,8 +135,8 @@ STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/tv/media/'
+MEDIA_ROOT = "/var/www/html/tv/media"
 
 
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
